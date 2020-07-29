610e436b916f331354ade23c7c66f4659fc3ba58
Natalie Pina <natalie.a.pina@gmail.com>
2020-07-29 17:24:52 -0500

    Updates

---
+++ precache-manifest.06ff7cfb2d8a623c88eb7a683e8a1874.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1e4a3f3649a8f8ae7b4ac878185882",
+    "url": "/GrowersBrains/index.html"
+  },
+  {
+    "revision": "2d86136d076e7969555f",
+    "url": "/GrowersBrains/static/css/main.a8ba28e6.chunk.css"
+  },
+  {
+    "revision": "5a012ab73749b71c3152",
+    "url": "/GrowersBrains/static/js/2.215003fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GrowersBrains/static/js/2.215003fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d86136d076e7969555f",
+    "url": "/GrowersBrains/static/js/main.d44d6cf4.chunk.js"
+  },
+  {
+    "revision": "3a19251c2e1e4c9ad252",
+    "url": "/GrowersBrains/static/js/runtime-main.cbf5ca97.js"
+  },
+  {
+    "revision": "884e0ba85d74e36b9984814bed4ef03b",
+    "url": "/GrowersBrains/static/media/bestReviewed.884e0ba8.jpg"
+  },
+  {
+    "revision": "544a4d952171c280e683fd290034ed4d",
+    "url": "/GrowersBrains/static/media/contest.544a4d95.jpg"
+  },
+  {
+    "revision": "9c01ed45eda16c784490f4ee26f1e563",
+    "url": "/GrowersBrains/static/media/dailyGrow.9c01ed45.jpg"
+  },
+  {
+    "revision": "ccbe5658b628cd1f583f8dd4b8344d0c",
+    "url": "/GrowersBrains/static/media/greenhouse.ccbe5658.jpg"
+  },
+  {
+    "revision": "28ba59f971038074869cc9f3008f0f4f",
+    "url": "/GrowersBrains/static/media/howTo.28ba59f9.jpg"
+  },
+  {
+    "revision": "fe4645f3f2ea8e7f6ebd5f24e68e446c",
+    "url": "/GrowersBrains/static/media/ranking.fe4645f3.jpg"
+  }
+]);
