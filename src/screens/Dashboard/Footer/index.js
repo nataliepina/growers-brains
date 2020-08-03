fe7d966673245c07b31e28d022e1e0aacc0369e0
Gajendra <gajendra.wbb@gmail.com>
2020-08-03 14:48:33 +0530

    fix review comments

--- src/screens/Dashboard/Footer/index.js
+++ src/screens/Dashboard/Footer/index.js
@@ -6,11 +6,11 @@ import {
 import './style.scss';
 
 export default () => (
-  <section id="footer">
+  <footer className="footer">
     <div className="container">
       <div className="top">
-        <p className="contactText">Contact</p>
-        <div className="contactInfo">
+        <p className="contact-text">Contact</p>
+        <div className="contact-info">
           <p className="phone">+1-234-567-8900</p>
           <p className="email">info@growersbrains.com</p>
         </div>
@@ -19,24 +19,49 @@ export default () => (
       </div>
       <div className="h-line" />
       <div className="bottom">
-        <div className="bottomLeft">
-          <ul className="footerList">
+        <div className="bottom-left">
+          <ul className="footer-list">
             <li><button href="#">Help</button></li>
             <li><button href="#">Terms</button></li>
             <li><button href="#">Privacy Policy</button></li>
           </ul>
           <p className="copyright"><span dangerouslySetInnerHTML={{ "__html": "&copy;" }} />2020 Growers Brains</p>
         </div>
-        <div className="socialIcons">
-          <ul>
-            <li><button><FontAwesomeIcon icon={faFacebook} size="lg" /></button></li>
-            <li><button><FontAwesomeIcon icon={faLinkedin} size="lg" /></button></li>
-            <li><button><FontAwesomeIcon icon={faTwitter} size="lg" /></button></li>
-            <li><button><FontAwesomeIcon icon={faYoutube} size="lg" /></button></li>
-            <li><button><FontAwesomeIcon icon={faInstagram} size="lg" /></button></li>
+        <div className="social-icons">
+          <ul className="social-list__items">
+            <li class="social-list__item">
+              <a href="http://facebook.com" aria-hidden="true" class="social-list__link" target="_blank" rel="noopener" alt="go to Instagram">
+                <span class="sr-only">Opens new window</span>
+                <i class="fab fa-facebook fa-2x"></i>
+              </a>
+            </li>
+            <li class="social-list__item">
+              <a href="http://linkedin.com" aria-hidden="true" class="social-list__link" target="_blank" rel="noopener" alt="go to LinkedIn">
+                <span class="sr-only">Opens new window</span>
+                <i class="fab fa-linkedin fa-2x"></i>
+              </a>
+            </li>
+            <li class="social-list__item">
+              <a href="http://twitter.com" aria-hidden="true" class="social-list__link" target="_blank" rel="noopener" alt="go to Twitter">
+                <span class="sr-only">Opens new window</span>
+                <i class="fab fa-twitter fa-2x"></i>
+              </a>
+            </li>
+            <li class="social-list__item">
+              <a href="http://youtube.com" aria-hidden="true" class="social-list__link" target="_blank" rel="noopener" alt="go to Youtube">
+                <span class="sr-only">Opens new window</span>
+                <i class="fab fa-youtube fa-2x"></i>
+              </a>
+            </li>
+            <li class="social-list__item">
+              <a href="http://instagram.com" aria-hidden="true" class="social-list__link" target="_blank" rel="noopener" alt="go to Instagram">
+                <span class="sr-only">Opens new window</span>
+                <i class="fab fa-instagram fa-2x"></i>
+              </a>
+            </li>
           </ul>
         </div>
       </div>
     </div>
-  </section>
+  </footer>
 );
--- src/screens/Dashboard/Subscribe/index.js
+++ src/screens/Dashboard/Subscribe/index.js
@@ -3,12 +3,12 @@ import ellipseImg from '../assets/ellipse.png';
 import './style.scss';
 
 export default () => (
-  <section id="subscribe">
+  <section className="subscribe">
     <div className="content">
       <h2 className="title">Subscribe for Daily Updates</h2>
-      <div className='inputContainer'>
-        <input id='email' type='text' className="inputEmail" placeHolder="Enter Email" />
-        <button id='submit' className="subscribeBtn" >
+      <div className='input-container'>
+        <input id='email' type='text' className="input-email" placeholder="Enter Email" />
+        <button id='submit' className="subscribe-btn" >
           Subscribe
         </button>
       </div>
