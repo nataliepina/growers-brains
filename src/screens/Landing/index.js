bed65214ce8451e5cc9f871162dc6963f9d2b64f
Justin Nguyen <nuggetnchill@gmail.com>
2020-08-14 11:55:59 -0500

    added HeroSection

--- src/screens/Landing/index.js
+++ src/screens/Landing/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import InfoSection from './InfoSection';
-import Subscribe from './Subscribe';
-import Footer from '../../components/common/Footer';
+import React from "react";
+import InfoSection from "./InfoSection";
+import Subscribe from "./Subscribe";
+import Footer from "../../components/common/Footer";
+import HeroSection from "./HeroSection";
 
 const Dashboard = () => {
   return (
     <>
+      <HeroSection />
       <InfoSection />
       <Subscribe />
       <Footer />
     </>
   );
-}
+};
 
 export default Dashboard;
