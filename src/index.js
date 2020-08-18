e9934aff2eb88d57992a021910788195b67b602f
Anatoly <rtp457@gmail.com>
2020-08-18 17:19:33 +0300

    adding redux logic

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {PersistGate} from 'redux-persist/integration/react';
+import {Provider} from 'react-redux';
+import {store,persistor} from './redux/store';
 
 ReactDOM.render(
 	<React.StrictMode>
+	<Provider store={store}>
+		<PersistGate persistor={persistor}>
 		<App />
+		</PersistGate>
+		</Provider>	
 	</React.StrictMode>,
 	document.getElementById('root')
 );
